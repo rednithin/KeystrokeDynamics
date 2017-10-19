cf3a13ccc2741438e71e9a8a1bc0ada0fbfa3012
rednithin <reddy.nithinpg@live.com>
2017-10-19 19:40:04 +0530

    Added production static file serving code.

--- index.js
+++ index.js
@@ -9,4 +9,18 @@ app.get('/', (req, res) => {
   res.send({'name': 'Hello'})
 })
 
+if (config.ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/dist'))
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path')
+  console.log(path)
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+  })
+}
+
 app.listen(config.PORT)
