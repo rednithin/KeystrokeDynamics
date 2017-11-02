ab87d95de0b80cd6dec2426f83e36400ba49da61
rednithin <reddy.nithinpg@live.com>
2017-11-02 20:08:43 +0530

    Added User Profile Page.

--- client/src/components/User/Profile.vue
+++ client/src/components/User/Profile.vue
@@ -1,18 +1,66 @@
 <template>
   <warning v-if="!$store.state.isUserLoggedIn">
   </warning>
-  <v-layout justify-center align-center v-else>
-    <v-flex>
-      Welcome <span>{{ $store.state.user.name }}</span> !!!
+  <v-layout column v-else>
+    <v-flex xs12 offset-md3 md6>
+      <panel title="My Profile">
+        <v-text-field
+          class="text-field"
+          label="Name"
+          readonly
+          v-model="$store.state.user.name"
+          @input="tapThat"
+          @blur="finish"
+        ></v-text-field>
+        <v-text-field
+          class="text-field"
+          label="Age"
+          readonly
+          v-model="$store.state.user.age"
+        ></v-text-field>
+        <v-text-field
+          v-model="$store.state.user.gender"
+          label="Gender"
+          readonly
+          class="text-field"
+        ></v-text-field>
+        <v-text-field
+          class="text-field"
+          label="Phone"
+          readonly
+          v-model="$store.state.user.phone"
+        ></v-text-field>
+        <v-text-field
+          class="text-field"
+          label="Email"
+          readonly
+          v-model="$store.state.user.email"
+        ></v-text-field>
+        <v-text-field
+          class="text-field"
+          label="Status"
+          readonly
+          v-model="$store.state.user.status"
+        ></v-text-field>
+        <v-text-field
+          class="text-field"
+          label="Objective"
+          readonly
+          v-model="$store.state.user.objective"
+          multi-line
+        ></v-text-field>
+      </panel>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
+import Panel from '@/components/Panel'
 import Warning from '@/components/Warning'
 export default {
   components: {
-    Warning
+    Warning,
+    Panel
   }
 }
 </script>
