0bf67bc1a257950bf4e420a3f83b0779c03870ea
rednithin <reddy.nithinpg@live.com>
2017-11-02 20:11:51 +0530

    Added View Profile Page.

--- client/src/components/User/ViewProfile.vue
+++ client/src/components/User/ViewProfile.vue
@@ -1,14 +1,61 @@
 <template>
   <warning v-if="!$store.state.isUserLoggedIn">
   </warning>
-  <v-layout justify-center align-center v-else>
-    <v-flex>
-      This is <span>{{ user.name }}'s</span> Profile Page!!!
+  <v-layout column v-else>
+    <v-flex xs12 offset-md3 md6>
+      <panel :title="user.name + '\'s Profile'">
+        <v-text-field
+          class="text-field"
+          label="Name"
+          readonly
+          v-model="user.name"
+          @input="tapThat"
+          @blur="finish"
+        ></v-text-field>
+        <v-text-field
+          class="text-field"
+          label="Age"
+          readonly
+          v-model="user.age"
+        ></v-text-field>
+        <v-text-field
+          v-model="user.gender"
+          label="Gender"
+          readonly
+          class="text-field"
+        ></v-text-field>
+        <v-text-field
+          class="text-field"
+          label="Phone"
+          readonly
+          v-model="user.phone"
+        ></v-text-field>
+        <v-text-field
+          class="text-field"
+          label="Email"
+          readonly
+          v-model="user.email"
+        ></v-text-field>
+        <v-text-field
+          class="text-field"
+          label="Status"
+          readonly
+          v-model="user.status"
+        ></v-text-field>
+        <v-text-field
+          class="text-field"
+          label="Objective"
+          readonly
+          v-model="user.objective"
+          multi-line
+        ></v-text-field>
+      </panel>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
+import Panel from '@/components/Panel'
 import Warning from '@/components/Warning'
 import UserServices from '@/services/UserServices'
 export default {
@@ -18,7 +65,8 @@ export default {
     }
   },
   components: {
-    Warning
+    Warning,
+    Panel
   },
   async mounted () {
     const userId = this.$store.state.route.params.userId
